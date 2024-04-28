d7885f13748029bacfdd5f6ade0f8a61968ad9d1
Pintó <marti.pinto@icgc.cat>
2024-04-28 20:00:57 +0200

    versio inicial

---
+++ cdn/7/widgets/EstiloKiosko/dist/runtime/widget.js
@@ -0,0 +1,9 @@
+System.register(["jimu-core"],(function(e,t){var r={};return{setters:[function(e){r.css=e.css,r.jsx=e.jsx}],execute:function(){e((()=>{var e={891:e=>{"use strict";e.exports=r}},t={};function o(r){var s=t[r];if(void 0!==s)return s.exports;var n=t[r]={exports:{}};return e[r](n,n.exports,o),n.exports}o.d=(e,t)=>{for(var r in t)o.o(t,r)&&!o.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},o.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),o.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.p="";var s={};return o.p=window.jimuConfig.baseUrl,(()=>{"use strict";o.r(s),o.d(s,{__set_webpack_public_path__:()=>i,default:()=>n});var e=o(891);const t=e.css`
+    font-size:25px;
+    color: green;
+    text-align: center;   
+`,r=e.css`
+    font-size:15px;
+    color: white;
+    text-align: center;    
+`,n=o=>(0,e.jsx)("div",{className:"w-100 bg-dark text-light"},(0,e.jsx)("p",{css:t},"Kioscos"),(0,e.jsx)("p",{css:r},"Renovación de licencia"));function i(e){o.p=e}})(),s})())}}}));
